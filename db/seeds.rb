b6fedb862d5fc497a34092cf2ad4b9fa45e6b654
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-22 15:15:13 -0500

    init commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Poster.create(  
+  name: "REGRET",
+  description: "Hard work rarely pays off.",
+  price: 89.00,
+  year: 2018,
+  vintage: true,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"  
+)
+
+Poster.create(
+  name: "FAILURE",
+  description: "Why bother trying, its probably not worth it.",
+  price: 68.00,
+  year: 2019,
+  vintage: true,
+  img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d"
+)
+
+Poster.create(  
+  name: "MEDIOCRATY",
+  description: "Dreams are just dreams.",
+  price: 127.00,
+  year: 2021,
+  vintage: false,
+  img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8"
+)
