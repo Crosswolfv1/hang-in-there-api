e47fd60cdd1afbb2c5e546b39ec50b716c9ba71e
Paul Knapp <ppankluap@gmail.com>
2024-10-23 15:44:05 -0500

    Feat: create method to format all posters to proper json.

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -15,4 +15,22 @@ class PosterSerializer
   }
   }
   end
+
+  def self.format_posters(posters)
+    {
+      data: 
+        posters.map do |poster| 
+          {id: poster.id,
+          type: "poster",
+          attributes: {
+          name: poster.name,
+          description: poster.description,
+          price: poster.price,
+          year: poster.year,
+          vintage: poster.vintage,
+          img_url: poster.img_url
+        }}
+        end
+    }
+  end
 end
