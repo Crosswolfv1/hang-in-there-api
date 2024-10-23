02cbe8611bdc893fe330729c9063608dacc7c552
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-23 17:14:23 -0500

    feat: added ability to send get reqwuests with a sort query

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::PostersController < ApplicationController
 
   def index
     posters = Poster.all
+    posters = sort_posters(posters)
     render json: PosterSerializer.format_posters(posters)
   end
 
@@ -23,5 +24,19 @@ class Api::V1::PostersController < ApplicationController
 
   def posters_params
     params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url )
+  end
+
+  def sort_posters(scope)
+    if (order = params.dig(:query, :sort))
+      column, direction = order.split(" ")
+
+      if column.presence_in(%w[created_at]) && direction.presence_in(%w[asc desc])
+        scope.order("#{column} #{direction}")
+      else
+        []
+      end
+    else
+      scope.order(created_at: :desc)
+    end
   end
 end
