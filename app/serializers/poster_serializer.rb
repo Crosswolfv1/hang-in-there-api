f769589a500de0914d19fd3bd0f54886f715e2a4
Paul Knapp <ppankluap@gmail.com>
2024-10-25 11:24:33 -0500

    Feat: add method to return id as an integer.

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -2,4 +2,10 @@ class PosterSerializer
   include JSONAPI::Serializer
   set_type :posters
   attributes :name, :description, :price, :year, :vintage, :img_url
-end
+
+  def self.record_hash(record, fieldset, includes_list, params)
+    hash = super
+    hash[:id] = record.id.to_i
+    hash
+  end
+end 
